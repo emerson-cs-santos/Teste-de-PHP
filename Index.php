98772f8ebb89d6637fbce670daab96cfb78fa83a
Emerson Costa <syphonemerson@yahoo.com.br>
2019-08-12 17:17:18 -0300

    Teste de conexão ao SQL SERVER

--- Index.php
+++ Index.php
@@ -2,5 +2,31 @@
 
 echo('teste');
 
+$serverName = "SERVERNAME"; 
+$uid = "sa";   
+$pwd = "g";  
+$databaseName = "VolpeModelo_ERPDesenv"; 
+
+$connectionInfo = array( "UID"=>$uid,                            
+                         "PWD"=>$pwd,                            
+                         "Database"=>$databaseName); 
+
+/* Connect using SQL Server Authentication. */  
+$conn = sqlsrv_connect( $serverName, $connectionInfo); 
+
+/* Execute the query. */  
+
+$stmt = sqlsrv_query( $conn, $tsql);  
+
+if ( $stmt )  
+{  
+     echo "Statement executed.<br>\n";  
+}   
+else   
+{  
+     echo "Error in statement execution.\n";  
+     die( print_r( sqlsrv_errors(), true));  
+}   
+
 ?>
 
